dec18722bce8c6e69f1dc09cf9930858eb39774e
Triad01 <luckypee01@gmail.com>
2023-11-03 19:58:58 +0100

    all

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for char_code in range(ord('z'), ord('a') - 1, -1):
+    char = chr(char_code)
+    if (char_code - ord('z')) % 2 == 0:
+        # Print lowercase character
+        print(char, end='')
+    else:
+        # Print uppercase character
+        print(char.upper(), end='')
