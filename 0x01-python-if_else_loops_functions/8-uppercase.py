a0768d258c964e19452ebc1ae6c3edee5c4cd94f
Triad01 <luckypee01@gmail.com>
2023-11-03 17:35:46 +0100

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,8 @@
 def uppercase(s):
     result = ""
     for char in s:
-        # Check if the character is a lowercase letter (ASCII values between 97 and 122).
-        if ord('a') <= ord(char) <= ord('z'):
-            # Convert the lowercase character to uppercase (ASCII value difference is 32).
-            result += chr(ord(char) - 32)
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - (ord('a') - ord('A')))
         else:
             result += char
-    print(result)
+    print(result, end="\n")
