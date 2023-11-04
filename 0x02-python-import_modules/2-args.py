9b0ef6ab1f8acb844c31f432b58aff593e68afd7
Triad01 <luckypee01@gmail.com>
2023-11-04 12:16:47 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+
+my_list = sys.argv
+
+my_list.pop(0)
+
+list_length = len(my_list)
+
+arg = "arguments:"
+
+if list_length == 1:
+
+    arg = "argument:"
+
+elif list_length == 0:
+
+    arg = "arguments."
+
+print(f"{list_length} {arg}")
+
+length = list_length + 1
+
+for num in range(list_length):
+
+    print(f"{num + 1}: {my_list[num]}")
