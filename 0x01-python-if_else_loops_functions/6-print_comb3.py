c32dd897d8e8723bb82a765a8b4b7ec696bcf346
Triad01 <luckypee01@gmail.com>
2023-11-06 11:57:42 +0100

    modified

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
-for tens in range(10):
-    for ones in range(tens + 1, 10):
-        if tens == 8 and ones == 9:
-            print("{:d}{:d}".format(tens, ones))
-        else:
-            print("{:d}{:d}, ".format(tens, ones), end='')
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(num), end=" ")
+            else:
+                print("{:d}".format(num))
