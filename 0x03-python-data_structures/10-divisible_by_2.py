aa7921a9868a04fed5ce6b3abb15580e82857039
Triad01 <luckypee01@gmail.com>
2023-11-06 10:18:46 +0100

    task 10 modified

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
 
-    new_list = [num % 2 == 0 for num in my_list]
+    new_list = []
+
+    for num in my_list:
+
+        is_divisible = True
+
+        new_list.append(is_divisible)
 
     return new_list
