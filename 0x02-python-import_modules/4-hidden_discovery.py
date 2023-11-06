a2c8627b3514b228fd617fc2637303cebaa8d016
Triad01 <luckypee01@gmail.com>
2023-11-06 11:19:31 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-name_list = dir(hidden_4)
-
-for name in name_list:
-    if not name.startwith("__"):
-        print(name)
+if __name__ == "__main__":
+    import hidden_4
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != '__':
+            print("{}".format(name))
