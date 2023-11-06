1892b46858849f6e338568a7b7d21b5e98db2e44
Triad01 <luckypee01@gmail.com>
2023-11-06 10:12:51 +0100

    task 10

--- 0x03-python-data_structures/10-main.py
+++ 0x03-python-data_structures/10-main.py
@@ -8,12 +8,3 @@ i = 0
 while i < len(list_result):
     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
     i += 1
-
-guillaume@ubuntu:~/0x03$ ./10-main.py
-0 is divisible by 2
-1 is not divisible by 2
-2 is divisible by 2
-3 is not divisible by 2
-4 is divisible by 2
-5 is not divisible by 2
-6 is divisible by 2
