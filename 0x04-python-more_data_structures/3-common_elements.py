9846c7bd40b59f18275824dcd9b2d02855459cd0
Triad01 <luckypee01@gmail.com>
2023-11-08 15:28:48 +0100

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
 
-    for item in set_1:
-
-        if item in set_2:
-
-            common_element = {item}
-
+    common_element = set_1 & set_2
+    
     return common_element
