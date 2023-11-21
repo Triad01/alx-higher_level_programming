94d5921183c11aeae7fbcd333fb7d323b8164ed8
Triad01 <luckypee01@gmail.com>
2023-11-21 13:40:27 +0100

    doc string for task 0 class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" This module defines the square class
+
+    This class represents a square with no attributes or methods at the moment
+"""
+
+
 class Square:
     """
     This is a brief description of the class
