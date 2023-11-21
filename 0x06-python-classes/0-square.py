c84b9d9778f536489cc3e5d46533597de80780dd
Triad01 <luckypee01@gmail.com>
2023-11-21 13:35:57 +0100

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """ This is a brief description of the class
+    """
+    This is a brief description of the class
 
-        It is an empty class that defines a square
+    Attributes:
+        No attributes at the moment
     """
     pass
