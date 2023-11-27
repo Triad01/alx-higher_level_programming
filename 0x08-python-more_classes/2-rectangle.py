ac2908b22d1de9b67ca86d63dc9cd2cfc8ee5d92
Triad01 <luckypee01@gmail.com>
2023-11-27 15:45:44 +0100

    modified public instance method for perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
         return area
 
     def perimeter(self):
-        if self.__width or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             perimeter = 0
         else:
             perimeter = int(self.__width * 2) + int(self.__height * 2)
