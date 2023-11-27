395a29dfb9d43db56ab20930b507bde100f1810c
Triad01 <luckypee01@gmail.com>
2023-11-27 15:12:13 +0100

    task 1

--- 0x08-python-more_classes/del.py
+++ 0x08-python-more_classes/del.py
@@ -12,42 +12,29 @@ class Rectangle:
         height: height of the rectangle
     """
     def __init__(self, width=0, height=0):
-
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """sets the width of the object"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets the width of the object"""
         if not isinstance(value, int):
-
             raise TypeError("width must be an integer")
-
         if value < 0:
-
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     @property
     def height(self):
-        """gets the height of the object"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height of the object"""
         if not isinstance(value, int):
-
             raise TypeError("height must be an integer")
-
         if value < 0:
-
             raise ValueError("height must be >= 0")
-
         self.__height = value
