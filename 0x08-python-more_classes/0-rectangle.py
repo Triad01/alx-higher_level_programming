74af24ed0af5a59f25ac97d9fc1e1fdf9e8d5033
Triad01 <luckypee01@gmail.com>
2023-11-27 14:16:48 +0100

    fixed task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""
-    This module defines an empty Rectangle class
+""" This module defines the rectangle class
+
+    This class represents a rectangle with no attributes or methods
 """
 
 
 class Rectangle:
+    """
+    This is a brief description of the class
+
+    Attributes:
+        No attributes at the moment
+    """
     pass
