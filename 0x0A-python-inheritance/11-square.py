7ce7c4973e63fa95c3b5e3039d5b0d984206299f
Triad01 <luckypee01@gmail.com>
2023-12-04 15:13:11 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,42 +1,11 @@
 #!/usr/bin/python3
 """ This module contains some classeses with some methods"""
-
-
-class BaseGeometry:
-    def area(self):
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-    def area(self):
-        return self.__width * self.__height
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
+    """ingerits from rectangle"""
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
-
-    def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+        super().__init__(size, size)
