2938afdf11fbf18163c3cefa92f612f7d74e2857
Triad01 <luckypee01@gmail.com>
2023-12-05 14:29:12 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """reads file and prints it to stdout"""
-    with open(filename, "w+") as my_file:
-        my_file.write("""We offer a truly innovative approach to education:
-focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers.
-
-A school every software engineer would have dreamt of!
-guillaume@ubuntu:~/0x0B$ ./0-main.py
-We offer a truly innovative approach to education:
-focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers.""")
-        my_file.seek(0)
+    with open(filename, "r") as my_file:
         data = my_file.read()
         print(data)
