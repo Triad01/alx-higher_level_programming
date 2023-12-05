0249141e1bcf988fc33cfc001432e66b491bee6a
Triad01 <luckypee01@gmail.com>
2023-12-05 14:37:11 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """reads file and prints it to stdout"""
     with open(filename, "r") as my_file:
         data = my_file.read()
-        print(data)
+        print(data, end="")
