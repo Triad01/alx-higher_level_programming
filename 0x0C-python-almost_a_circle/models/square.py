e736b3e2012b71f1033fab3c1312426abe65ade7
Triad01 <luckypee01@gmail.com>
2023-12-11 21:09:28 +0100

    modified square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,60 +1,50 @@
 #!/usr/bin/python3
-""" module contains the Square class """
+""" New class Square """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """ Class Square that inherist from Rectangle """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """class constructor"""
+        """ Class constructor """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
+    """ Returns printable string representation
+    of an instance """
     def __str__(self):
-        """returns printable string representatin"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return ("[{}] ({}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x,
+            self.y, self.size))
 
+    """ Size getter and setter """
     @property
     def size(self):
-        """getter function for the size"""
-        return f"{self.width}"
+        return (self.width)
 
     @size.setter
     def size(self, value):
-        """setter for the size"""
-
-        if not isinstance(value, int):
-
-            raise TypeError("width must be an integer")
-
-        if value <= 0:
-
-            raise ValueError("width must be > 0")
-
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """update function"""
+        """ This method assigns an argument to each attribute """
         if args:
-            if len(args) > 0:
-                self.id = args[0]
-            if len(args) > 1:
-                self.size = args[1]
-            if len(args) > 2:
-                self.x = args[2]
-            if len(args) > 3:
-                self.y = args[3]
+            for argmts in range(len(args)):
+                if argmts == 0:
+                    self.id = args[argmts]
+                if argmts == 1:
+                    self.size = args[argmts]
+                if argmts == 2:
+                    self.x = args[argmts]
+                if argmts == 3:
+                    self.y = args[argmts]
 
-        elif kwargs:
-
+        else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """returns dictionary representatin of object"""
-        return {
-            "id": self.id,
-            "x": self.x,
-            "size": self.width,
-            "y": self.y
-        }
+        """ Returns the dictionary representation of a Rectangle"""
+        return ({'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y})
