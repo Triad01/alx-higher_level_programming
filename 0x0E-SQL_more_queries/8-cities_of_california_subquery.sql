1a23679d04dca80c36b89ab913992d13d55c89b3
Triad01 <luckypee01@gmail.com>
2024-01-17 17:05:57 +0100

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- script lists cities of califonia that can be found in the database
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id from states WHERE name = "California")
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY id;
