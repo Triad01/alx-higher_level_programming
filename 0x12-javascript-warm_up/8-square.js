b68cecc902dcb17c73338873a7f4cb5d3a686fe5
Triad01 <luckypee01@gmail.com>
2024-02-12 15:09:21 +0100

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,6 +5,6 @@ const squareSize = process.argv[2];
 if (!Number(squareSize)) console.log('Missing size');
 else {
   for (let i = 0; i < squareSize; i++) {
-    console.log('x'.repeat(squareSize));
+    console.log('X'.repeat(squareSize));
   }
 }
