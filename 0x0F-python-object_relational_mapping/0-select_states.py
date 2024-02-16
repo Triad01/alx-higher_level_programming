8b91b7b688ed791b5769368468f666e99c2d2a11
Triad01 <luckypee01@gmail.com>
2024-02-16 11:50:12 +0000

    pycodestyle compliant

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" Module contains script that lists all states from the database hbtn_0e_0_usa """
+""" Module contains script that
+    lists all states from the
+    database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    #make connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    # make connection to the database
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-        
     # Clean up process
     cursor.close()
     db.close()
-    
-        
