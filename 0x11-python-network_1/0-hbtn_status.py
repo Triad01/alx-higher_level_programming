c7976b5f43d1fc927d639008c553e58207cfceab
Triad01 <luckypee01@gmail.com>
2024-03-11 19:43:50 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Script fetches a url using the urllib package"""
+"""
+    Module implementing url fetch
+    using urllib module
 
+"""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
+url = "https://alx-intranet.hbtn.io/status"
 if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        print("Body response:")
-        print("\t - type: {}".format(type(body)))
-        print("\t - content: {}".format(body))
-        print("\t - utf8 content:".format(body.decode("utf-8")))
+        print('Body response:')
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+        print(f'\t- utf8 content: {body.decode("utf-8")}')
