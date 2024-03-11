093210594d4e5e8d7ef99df354e8ce8a053c6ad0
Triad01 <luckypee01@gmail.com>
2024-03-11 21:57:34 +0100

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,3 +10,5 @@ if __name__ == "__main__":
     response = requests.get(url)
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
+    else:
+        print(response)
