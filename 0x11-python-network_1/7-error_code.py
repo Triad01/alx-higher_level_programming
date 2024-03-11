2a71be228455d818fac3d3b8fbe2f5d8bf85de67
Triad01 <luckypee01@gmail.com>
2024-03-11 22:00:22 +0100

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-# script takes in a URL, sends a request to the URL
-# and displays the body of the response.
+"""
+    Module to implement a script that displays error code
+    using requests package
 
-import sys
+"""
 import requests
+import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
+
     response = requests.get(url)
+
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
