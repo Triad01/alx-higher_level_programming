ed178c62c714bc6360d998f3cfe8e6641b3b610d
Triad01 <luckypee01@gmail.com>
2024-05-21 19:38:29 +0100

    completed all mandatory tasks

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script displays the status code of a get request
+const request = require('request');
+
+// Extract the URL from command-line arguments
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log('code:', response.statusCode);
+});
